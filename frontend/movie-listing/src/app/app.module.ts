71e011cc04efa365853eab26081e890f2125bd9d
Narendra Kamath <narendrakamath@192.168.1.6>
2024-10-25 01:02:55 +0530

    components

--- frontend/movie-listing/src/app/app.module.ts
+++ frontend/movie-listing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ListingComponent } from './components/listing/listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BannerComponent,
+    ListingComponent
   ],
   imports: [
     BrowserModule,
